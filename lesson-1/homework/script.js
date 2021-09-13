a305aecc3deded95b4e2237a1267cc941cf02d70
Alex261191 <limesss1991@yandex.ru>
2021-09-13 20:54:13 +0300

    homework

--- lesson-1/homework/script.js
+++ lesson-1/homework/script.js
@@ -2,14 +2,31 @@
     Создать столько переменных сколько типов данных
     Вывести занчение и тип переменной в console.log
 */
-
+let number = 4;
+console.log(number);
+let bigint = 100000000000000000000000n;
+console.log(bigint);
+let string = "google";
+console.log(string);
+let flse = 20<10;
+console.log(flse);
+let nul = null;
+console.log(nul);
+let undef;
+console.log(undef);
+let obj = {t:45};
+console.log(obj);
+let symb = Symbol([3234]);
+console.log(symb);
 /*
     Создать переменные a = 22 и b = 7. 
     Найти остаток от деления a на b, вывести в alert или console.log
 
     * Переменные присваиваются через prompt
 */
-
+let a = 22
+let b = 7
+console.log(a % b)
 /*
     Создать переменные x и y с рандомными числами. 
     Найти остаток от деления x на y
@@ -17,7 +34,9 @@
 
     - использовать Math https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math
 */
-
+let x = 12
+let y = 15
+console.log()
 /*
     Создать переменную с результатом 10 в степени 10
     - использовать Math https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math
