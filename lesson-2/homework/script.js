a70d037f4621541aba52ea46b4e80899b78bd2bc
AndreyVyrva <andrey.hedgehog@mail.ru>
2021-09-19 23:04:23 +0300

    AndreyV-lesson-2: added lesson-2 homework

--- lesson-2/homework/script.js
+++ lesson-2/homework/script.js
@@ -5,9 +5,19 @@
 */
 
 const nums1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+console.log('Длина массива =', nums1.length);
 
-let sum;
-let multi;
+let sum=0;
+for (i=0; i<nums1.length; i++){
+  sum = sum + i;
+};
+console.log('Сумма чисел =', sum);
+
+let multi=1;
+for (i=1; i<nums1.length; i++){
+  multi = multi * i;
+}
+console.log('Произведение чисел =', multi);
 // code
 
 /*
@@ -16,9 +26,18 @@ let multi;
 */
 
 const nums2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
+console.log('Длина массива =', nums2.length);
 const even = [];
 const odd = [];
+for (i = 0; i < nums2.length; i++) {
+  if (nums2[i] % 2 === 0) {
+    even.push(nums2[i]);
+  } else {
+    odd.push(nums2[i]);
+  }
+}
+console.log('Массив чётных чисел =', even);
+console.log('Массив нечётных чисел =', odd);
 //code
 
 /*
@@ -28,8 +47,18 @@ const odd = [];
 
 const nums3 = [578, 894, 982, 78, 1223, 353, 123, 544, 429, 5423, 1914, 120];
 
-let max;
-let min;
+let max=nums3[0];
+let min=nums3[0];
+for (i=0; i<nums3.length; i++){
+  if (nums3[i]>max){
+    max=nums3[i]}
+}
+console.log('Самое большое число в массиве =', max);
+for (i=0; i<nums3.length; i++){
+  if (nums3[i]<min){
+    min=nums3[i]}
+}
+console.log('Самое маленькое число в массиве =', min);
 // code
 
 /*
@@ -60,30 +89,9 @@ const users = [
     email: "Nathan@yesenia.net",
   },
 ];
-
-/*
-    Дан объект {a: 1, b: 2, c: 3, d: 4, e: 5, f: 6}
-    
-    Создать массив с ключами обьекта (должен работать как Object.keys())
-    Создать массив со значениями обьекта (должен работать как Object.values())
-    
-    * Создать массив со формата [[propName1, value1], ...] (должен работать как Object.entries())
-*/
-
-const obj = {
-  a: 1,
-  b: 2,
-  c: 3,
-  d: 4,
-  e: 5,
-  f: 6,
-};
-
-const keys = [];
-// code
-
-const values = [];
-// code
-
-const entries = [];
-// code
+let key=prompt('Введите имя пользователя');
+for (i=0; i<users.length; i++){
+  if (key.toLowerCase() === users[i].username.toLowerCase()){
+    console.log(`Name: ${users[i].name}, Email: ${users[i].email}`);
+  }
+}
