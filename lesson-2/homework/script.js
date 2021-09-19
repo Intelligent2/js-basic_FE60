fcfe7cef715b6aa401c23e827120cfea2ba98105
Dzmitry Tabolich <dzmitry.tabolich@gmail.com>
2021-09-19 23:49:23 +0300

    Class work and home work

--- lesson-2/homework/script.js
+++ lesson-2/homework/script.js
@@ -6,9 +6,17 @@
 
 const nums1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-let sum;
-let multi;
-// code
+let sum = 0;
+let multi = 1;
+
+for (let i = 0; i < nums1.length; i++) {
+  sum += i;
+  if (i > 0) {
+    multi *= i;
+  }
+}
+console.log(sum);
+console.log(multi);
 
 /*
     Дан массив [0,1,2,3,4,5,6,7,8,9,10,11,12]
@@ -19,7 +27,15 @@ const nums2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 const even = [];
 const odd = [];
-//code
+for (let i = 0; i < nums2.length; i++) {
+  if (i % 2 === 0 && i) {
+    even[i] = i;
+  } else if (i % 2 && i) {
+    odd[i] = i;
+  }
+}
+console.log(even);
+console.log(odd);
 
 /*
     Дан массив [578,894,982,78,1223,353,123,544,429,5423,1914,120]
@@ -28,9 +44,18 @@ const odd = [];
 
 const nums3 = [578, 894, 982, 78, 1223, 353, 123, 544, 429, 5423, 1914, 120];
 
-let max;
-let min;
-// code
+let max = nums3[0];
+let min = nums3[0];
+
+for(let i = 0; i < nums3.length; i++) {
+  if (nums3[i] > max) {
+    max = nums3[i];
+  } else if (nums3[i] < min) {
+    min = nums3[i];
+  }
+}
+console.log(max);
+console.log(min);
 
 /*
     Дан обьект users
@@ -39,6 +64,8 @@ let min;
 
     * Добавить возможность поиска без учёта регистра
 */
+
+let userName = prompt("Ввести имя пользователя");
 
 const users = [
   {
@@ -61,6 +88,12 @@ const users = [
   },
 ];
 
+for (let key in users) {
+  if (users[key].name === userName) {
+    console.log(`User name: ${users[key].username}, User email: ${users[key].email}`);
+  }
+}
+
 /*
     Дан объект {a: 1, b: 2, c: 3, d: 4, e: 5, f: 6}
     
@@ -79,11 +112,11 @@ const obj = {
   f: 6,
 };
 
-const keys = [];
-// code
+const keys = Object.keys(obj);
+console.log(keys);
 
-const values = [];
-// code
+const values = Object.values(obj);
+console.log(values);
 
 const entries = [];
 // code
