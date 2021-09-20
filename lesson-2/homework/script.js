0dbb8351495063ddba1394e72d29ac85cb87922a
User-Victor <vitiashaferov221992@gmail.com>
2021-09-20 18:48:20 +0300

    added homework2

--- lesson-2/homework/script.js
+++ lesson-2/homework/script.js
@@ -6,9 +6,18 @@
 
 const nums1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-let sum;
-let multi;
+let sum = 0;
+let multi = 1;
 // code
+for (let i = 0; i <= 12; i++){
+  sum += i;
+  if(i > 0){
+    multi *= i
+  }
+};
+console.log(sum);
+console.log(multi);
+
 
 /*
     Дан массив [0,1,2,3,4,5,6,7,8,9,10,11,12]
@@ -20,6 +29,16 @@ const nums2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 const even = [];
 const odd = [];
 //code
+for(let i = 0;i <= 12; i++){
+  if(i % 2 === 0){
+  even.push(i);
+  console.log(even);
+  }else{
+    odd.push(i);
+    console.log(odd);
+  }
+};
+
 
 /*
     Дан массив [578,894,982,78,1223,353,123,544,429,5423,1914,120]
@@ -31,7 +50,10 @@ const nums3 = [578, 894, 982, 78, 1223, 353, 123, 544, 429, 5423, 1914, 120];
 let max;
 let min;
 // code
-
+max = Math.max(578, 894, 982, 78, 1223, 353, 123, 544, 429, 5423, 1914, 120);
+min = Math.min(578, 894, 982, 78, 1223, 353, 123, 544, 429, 5423, 1914, 120);
+console.log(max);
+console.log(min);
 /*
     Дан обьект users
     В  окно prompt необходимо ввести имя пользователя
@@ -60,6 +82,10 @@ const users = [
     email: "Nathan@yesenia.net",
   },
 ];
+// let qst = prompt('Enter your name');
+// for (qst in users){
+// console.log(qst);
+// }
 
 /*
     Дан объект {a: 1, b: 2, c: 3, d: 4, e: 5, f: 6}
@@ -81,8 +107,8 @@ const obj = {
 
 const keys = [];
 // code
-
-const values = [];
+for(keys in obj);
+console.log(keys);
 // code
 
 const entries = [];
