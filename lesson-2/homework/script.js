79e90046ea5d8983911d113980b0bf67bfb4b46d
Maxim Lee <ml.js.dev@gmail.com>
2021-09-20 17:40:03 +0300

    lesson & homework 2

--- lesson-2/homework/script.js
+++ lesson-2/homework/script.js
@@ -6,9 +6,14 @@
 
 const nums1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-let sum;
-let multi;
-// code
+// let sum = 0;
+// let multi = 1;
+// for ( let i = 0; i < nums1.length; i++ ) {
+//   sum += nums1[i]
+//   if(nums1[i]) {
+//     multi *= nums1[i]
+//   }
+// }
 
 /*
     Дан массив [0,1,2,3,4,5,6,7,8,9,10,11,12]
@@ -16,10 +21,15 @@ let multi;
 */
 
 const nums2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
 const even = [];
 const odd = [];
-//code
+// for (let i = 0; i < nums2.length; i++ ) {
+//   if ( nums2[i]%2 ) { //how this part works?
+//     odd.push(nums2[i])
+//   } else {
+//     even.push(nums2[i])
+//   }
+// }
 
 /*
     Дан массив [578,894,982,78,1223,353,123,544,429,5423,1914,120]
@@ -28,10 +38,8 @@ const odd = [];
 
 const nums3 = [578, 894, 982, 78, 1223, 353, 123, 544, 429, 5423, 1914, 120];
 
-let max;
-let min;
-// code
-
+// let max = Math.max.apply(Math, nums3);
+// let min = Math.min.apply(Math, nums3);
 /*
     Дан обьект users
     В  окно prompt необходимо ввести имя пользователя
@@ -60,6 +68,13 @@ const users = [
     email: "Nathan@yesenia.net",
   },
 ];
+// let nameInput = prompt("Enter your name", '');
+// for (let i = 0; i < users.length; i++){
+//   if (nameInput == users[i].username) {
+//     console.log(`user name: ${users[i].username}, user email: ${users[i].email}`)
+//   }
+// }
+
 
 /*
     Дан объект {a: 1, b: 2, c: 3, d: 4, e: 5, f: 6}
@@ -79,11 +94,17 @@ const obj = {
   f: 6,
 };
 
-const keys = [];
-// code
+const keys = []
+for (const key in obj) {
+  keys.push(key)
+}
 
 const values = [];
-// code
+for (const val in obj) {
+  values.push(obj[val])
+}
 
 const entries = [];
-// code
+for (const ent in obj) {
+  entries.push([ent, obj[ent]])
+}
