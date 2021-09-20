1bd619945f0304b9de17688c2770be3c84837138
Филипп Коваленко <filippkovalenko@MacBook-Air-Filipp.local>
2021-09-20 18:28:36 +0300

    k_filipp-lesson-2: added lesson-2 homework

--- lesson-2/homework/script.js
+++ lesson-2/homework/script.js
@@ -6,8 +6,16 @@
 
 const nums1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-let sum;
-let multi;
+let sum = 0;
+let multi = 1;
+  for(let i = 0; i <= nums1.length; i++ ){
+    sum += i;
+    if(i > 0){
+      multi *= i;
+    }
+  }
+console.log(sum)
+console.log(multi)
 // code
 
 /*
@@ -19,6 +27,15 @@ const nums2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 const even = [];
 const odd = [];
+for(let i = 0; i < nums1.length; i++ ){
+  if(i % 2 === 0) {
+    even.push(i)
+  }else if(i % 2 === 1) {
+    odd.push(i)
+  }
+}
+console.log(even)
+console.log(odd)
 //code
 
 /*
@@ -28,8 +45,20 @@ const odd = [];
 
 const nums3 = [578, 894, 982, 78, 1223, 353, 123, 544, 429, 5423, 1914, 120];
 
-let max;
-let min;
+let max = nums3[0]
+let min = nums3[0]
+
+for(let i = 0; i < nums3.length; i++ ){
+  if(max < nums3[i]){
+    max = nums3[i]
+  }if(min > nums3[i]){
+    min = nums3[i]
+  }
+} 
+console.log(max)
+console.log(min)
+  
+
 // code
 
 /*
@@ -39,6 +68,7 @@ let min;
 
     * Добавить возможность поиска без учёта регистра
 */
+let userName = prompt("Ввести имя пользователя");
 
 const users = [
   {
@@ -59,7 +89,15 @@ const users = [
     username: "Samantha",
     email: "Nathan@yesenia.net",
   },
+ 
 ];
+for(let propName in users) {
+  if (users[propName].name === userName) {
+    console.log('User_name: '+ users[propName].username + ', ' + 'User_email: ' + users[propName].email);
+  }
+}
+
+
 
 /*
     Дан объект {a: 1, b: 2, c: 3, d: 4, e: 5, f: 6}
@@ -80,10 +118,15 @@ const obj = {
 };
 
 const keys = [];
-// code
-
 const values = [];
-// code
-
 const entries = [];
-// code
+for(let propName2 in obj){
+  keys.push(propName2)
+  values.push(obj[propName2])
+  entries.push([propName2,obj[propName2]])
+  //entries.push(`${propName2} ${obj[propName2]}`)
+}
+
+console.log(keys)
+console.log(values)
+console.log(entries)
