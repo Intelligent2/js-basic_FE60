aa01715b0e33492cae297b1c7e3e344c1714ccbf
Alexandra <alex_gorskaya1@mail.ru>
2021-09-20 22:08:49 +0300

    added classwork lesson-3

--- lesson-3/classwork/script.js
+++ lesson-3/classwork/script.js
@@ -3,6 +3,15 @@
     Функция должна возвращать самое большое число из переданных параметров
     Функция должна уметь работать с разным количетвом параметров
 */
+const getBiggest = function (...args) {
+    let max = args[0];
+for (let i = 0; i<args.length; i++ ){
+    if (max<args[i]) {
+max = args[i]
+    }
+}
+return max;
+}
 
 /*
     Создать функцию с именем duplicate
@@ -10,13 +19,25 @@
     Функция должна возратить удвоенный массив
     Пример: [1,2,3] => [1,2,3,1,2,3] 
 */
-
+const duplicate = function (args){
+args.push(...args);
+return args;
+}
 /*
     Создать функцию с именем isPalindrome
     Проверить является ли строка палиндромом
     Пример палиндромов: 'доход', 'шалаш'
 */
-
+const isPalindrome = function (word) {
+    let arr = word.split('').reverse().join('');
+    if (arr === word){
+        return true
+        } 
+        else {
+            return false
+        }
+    }
+    
 /*
     Создайте массив с десятью случайными элементами от 0 до 100;
     Создать функцию с именем replacer, которая будет заменять все “0” на строку “zero”;
@@ -24,4 +45,9 @@
     Пример: [1zero2, 53, “2zero”, 18, 22, “1zerozero”, 43, 57, “5zero”, 1];
 
     * для рандомного числа используйте Math.floor(Math.random() * 100)
-*/
+*/ 
+let arr = [];
+for (i=0; i<10;i++){
+    let a = Math.floor(Math.random() * 100);
+    arr.push(a);
+}
