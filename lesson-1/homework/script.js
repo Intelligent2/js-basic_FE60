ad4f0739c11cdce807b8ecd35b62b42aa9742961
Intelligent <intelligent@MacBook-Pro-Intelligent.local>
2021-09-23 20:16:19 +0300

    added lesson-1 & lesson-3 homework

--- lesson-1/homework/script.js
+++ lesson-1/homework/script.js
@@ -2,6 +2,23 @@
     Создать столько переменных сколько типов данных
     Вывести занчение и тип переменной в console.log
 */
+    let n = 32;
+    n = 33;
+
+    const bigInt = 8457289594n;
+
+    let string = "йо";
+    console.log("Йо");
+
+    let age = null;
+
+    let isGreater = 2 > 1;
+    alert (isGreater);
+
+    let age = 33;
+    age = undefined;
+    alert (age);
+
 
 /*
     Создать переменные a = 22 и b = 7. 
@@ -9,7 +26,9 @@
 
     * Переменные присваиваются через prompt
 */
-
+    let a = 22;
+    let b = 7;
+    alert( 22 / 7); 
 /*
     Создать переменные x и y с рандомными числами. 
     Найти остаток от деления x на y
@@ -17,16 +36,21 @@
 
     - использовать Math https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math
 */
-
+    let x ;
+    let y ;
+    Math.floor(Math.random() * max);
+    console.log( x / y);
 /*
     Создать переменную с результатом 10 в степени 10
     - использовать Math https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math
 */
-
+    Math.pow(10, 10);
 /*
     Создать переменную которая хранит квадратный корень из 245
     - использовать Math https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math
 */
+    let = 15.6524758425;
+    console.log(let);
 
 /*
     Создать переменные e и f. 
@@ -37,6 +61,7 @@
     * Переменные присваиваются через prompt
 */
 
+
 /*
     Создать переменные c и d. 
     Найти найдите модуль разности c и d
@@ -44,6 +69,9 @@
 
     * Переменные присваиваются через prompt
 */
+ let = c;
+ let = d;
+
 
 /*
     Вывести окно prompt с вопросом о имени
@@ -54,17 +82,27 @@
     Изменить значение переменной на любое другое
     Результат снова вывести в alert или console.log
  */
+    let name = prompt("Какое Имя?");
+    let lastname = prompt("Фамилия");
+    let age = prompt("сколько лет");
+    alert(`Меня зовут Ed Kuprianov 21`);
+    alert(`нет мне все таки 25`);
 
-/*
+    /*
     Создать обьект {name: '', lastName: '', age: null}
     Вывести окно prompt с вопросом о имени
     Вывести окно prompt с вопросом о фамилии
     Вывести окно prompt с вопросом о возрасте
     Создать переменную, которая содержит строку вида 'Меня зовут lastName name, мне age лет/год'
 */
-
+let name = prompt("Какое Имя?");
+let lastname = prompt("Фамилия");
+let age = prompt("сколько лет");
+alert(`Меня зовут Ed Kuprianov 21`);
 /*
     Вывести окно prompt с вопросом о возрасте
     В зависимости от ответа вывести alert с текстом “поздравляем, вам X полных лет”
     Вывести окно alert с утверждением совершеннолетний пользователь или нет
 */
+ let age = prompt("сколько живешь ? ");
+ alert(`Ого ${age} Поздравлем еще живой!`);
--- lesson-3/classwork/script.js
+++ lesson-3/classwork/script.js
@@ -1,21 +1,42 @@
+
 /*
+
     Создать функцию с именем getBiggest
     Функция должна возвращать самое большое число из переданных параметров
     Функция должна уметь работать с разным количетвом параметров
 */
+ /* const getBiggest = (...nums)=>{
+     let max=Infinity;
+             for (let i = 0; i < nums.length; i++){
+           (max=nums[i] > max) {    
+        max = nums[i];
+        }
+        }
+     return max;
 
+    }
 /*
     Создать функцию с именем duplicate
     Функция должна принимать массив
     Функция должна возратить удвоенный массив
     Пример: [1,2,3] => [1,2,3,1,2,3] 
 */
+    const duplicate = (array) =>{
+        array.push(...array);
+        return array;
+    }
+
 
 /*
     Создать функцию с именем isPalindrome
     Проверить является ли строка палиндромом
     Пример палиндромов: 'доход', 'шалаш'
 */
+function isPalindrome(s,i) {
+    return(i=i||0)<0||i>=s.length>>1||s[i]==s[s.length-1-i]&&isPalindrome(s,++i);
+   }
+   console.log(isPalindrome('test'));
+   console.log(isPalindrome('racecar'));
 
 /*
     Создайте массив с десятью случайными элементами от 0 до 100;
@@ -25,3 +46,19 @@
 
     * для рандомного числа используйте Math.floor(Math.random() * 100)
 */
+function randomArray(count, min, max) {
+    if (count > (max - min)) return;
+    var arr = [], t;
+  
+    while (count) {
+      t = Math.floor(Math.random() * (max - min) + min);
+      if (arr.indexOf(t) === -1) {
+        arr.push(t);
+        count--;
+      }
+    }
+    return arr;
+  }
+  console.log(randomArray(10, 1, 20));
+
+  
