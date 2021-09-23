e045ba7fdf0c675d8f7a751da253bda4c3299fc7
sevichok <sevichok41294@gmail.com>
2021-09-23 19:03:06 +0300

    classwork lesson 3

--- lesson-3/classwork/script.js
+++ lesson-3/classwork/script.js
@@ -4,19 +4,34 @@
     Функция должна уметь работать с разным количетвом параметров
 */
 
+const getBiggest = (...nums)=>{
+    let max=0;
+    for (i=0;i<nums.length;i++) {
+        if (nums[i]>max){
+            max=nums[i]
+        }
+    }
+  return max;
+}
 /*
     Создать функцию с именем duplicate
     Функция должна принимать массив
     Функция должна возратить удвоенный массив
     Пример: [1,2,3] => [1,2,3,1,2,3] 
+    
 */
+
+const duplicate = (res)=>{
+    res.push(...res);
+    return res ;
+}
 
 /*
     Создать функцию с именем isPalindrome
     Проверить является ли строка палиндромом
     Пример палиндромов: 'доход', 'шалаш'
 */
-
+const isPalindrome = (string)=> string===string.split('').reverse().join('');
 /*
     Создайте массив с десятью случайными элементами от 0 до 100;
     Создать функцию с именем replacer, которая будет заменять все “0” на строку “zero”;
@@ -25,3 +40,12 @@
 
     * для рандомного числа используйте Math.floor(Math.random() * 100)
 */
+const arrays = [];
+while (arrays.length < 10) {
+  const randomNum = Math.floor(Math.random() * 100);
+  if (arrays.includes(randomNum)) {
+    continue;
+  }
+  arrays.push(randomNum);
+}
+console.log(arrays);
