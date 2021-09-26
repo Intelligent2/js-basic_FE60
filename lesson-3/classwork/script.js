831b3a02d6a2dfdb044deee0e4c5a7e63d985d3b
sevichok <sevichok41294@gmail.com>
2021-09-26 21:19:05 +0300

    lesson 3 classwork done

--- lesson-3/classwork/script.js
+++ lesson-3/classwork/script.js
@@ -5,6 +5,7 @@
 */
 
 const getBiggest = (...nums)=>{
+
     let max=0;
     for (i=0;i<nums.length;i++) {
         if (nums[i]>max){
@@ -12,6 +13,7 @@ const getBiggest = (...nums)=>{
         }
     }
   return max;
+
 }
 /*
     Создать функцию с именем duplicate
@@ -31,7 +33,15 @@ const duplicate = (res)=>{
     Проверить является ли строка палиндромом
     Пример палиндромов: 'доход', 'шалаш'
 */
+/*
 const isPalindrome = (string)=> string===string.split('').reverse().join('');
+*/
+
+const isPalindrome = (string) => {
+   return string===string.split("").reverse().join("")
+}
+
+
 /*
     Создайте массив с десятью случайными элементами от 0 до 100;
     Создать функцию с именем replacer, которая будет заменять все “0” на строку “zero”;
@@ -39,13 +49,39 @@ const isPalindrome = (string)=> string===string.split('').reverse().join('');
     Пример: [1zero2, 53, “2zero”, 18, 22, “1zerozero”, 43, 57, “5zero”, 1];
 
     * для рандомного числа используйте Math.floor(Math.random() * 100)
-*/
-const arrays = [];
-while (arrays.length < 10) {
-  const randomNum = Math.floor(Math.random() * 100);
-  if (arrays.includes(randomNum)) {
+    
+    /*const arrays = [];
+    while (arrays.length < 10) {
+    const randomNum = Math.floor(Math.random() * 100);
+    if (arrays.includes(randomNum)) {
     continue;
+    }
+    arrays.push(randomNum);
+    }
+    console.log(arrays);
+
+*/
+
+const replacer = (arrays) => {
+  let newArray=[];
+  for (let i = 0; i < arrays.length; i++) {
+    if (arrays[i].toString().includes('0')){
+      let number = arrays[i].toString();
+      let string = '';
+        for (let k=0;k<number.length;k++){
+          if (number[k]==="0"){
+            string=string + "zero";
+          }
+          else {
+            string=string + number[k];
+          }
+        } 
+      newArray.push(string);
+    }
+    else newArray.push(arrays[i])
+    }
+    console.log(newArray)
+    return newArray
   }
-  arrays.push(randomNum);
-}
-console.log(arrays);
+
+ 
