771f2273e095bb1e0014615b038f88a73be5b4ce
Alexandra <alex_gorskaya1@mail.ru>
2021-09-26 11:58:52 +0300

    lasttask

--- lesson-3/classwork/script.js
+++ lesson-3/classwork/script.js
@@ -3,7 +3,6 @@
     Функция должна возвращать самое большое число из переданных параметров
     Функция должна уметь работать с разным количетвом параметров
 */
-<<<<<<< HEAD
 const getBiggest = function (...args) {
     let max = args[0];
 for (let i = 0; i<args.length; i++ ){
@@ -13,20 +12,18 @@ max = args[i]
 }
 return max;
 }
-=======
 
-const getBiggest = (...nums) => {
-  let max = -Infinity;
+// const getBiggest = (...nums) => {
+//   let max = -Infinity;
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > max) {
-      max = nums[i];
-    }
-  }
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] > max) {
+//       max = nums[i];
+//     }
+//   }
 
-  return max;
-};
->>>>>>> origin
+//   return max;
+// };
 
 /*
     Создать функцию с именем duplicate
@@ -34,25 +31,21 @@ const getBiggest = (...nums) => {
     Функция должна возратить удвоенный массив
     Пример: [1,2,3] => [1,2,3,1,2,3] 
 */
-<<<<<<< HEAD
 const duplicate = function (args){
 args.push(...args);
 return args;
 }
-=======
 
-const duplicate = (array) => {
-  array.push(...array);
-  return array;
-};
+// const duplicate = (array) => {
+//   array.push(...array);
+//   return array;
+// };
 
->>>>>>> origin
 /*
     Создать функцию с именем isPalindrome
     Проверить является ли строка палиндромом
     Пример палиндромов: 'доход', 'шалаш'
 */
-<<<<<<< HEAD
 const isPalindrome = function (word) {
     let arr = word.split('').reverse().join('');
     if (arr === word){
@@ -63,9 +56,6 @@ const isPalindrome = function (word) {
         }
     }
     
-=======
-
->>>>>>> origin
 /*
     Создайте массив с десятью случайными элементами от 0 до 100;
     Создать функцию с именем replacer, которая будет заменять все “0” на строку “zero”;
@@ -73,13 +63,26 @@ const isPalindrome = function (word) {
     Пример: [1zero2, 53, “2zero”, 18, 22, “1zerozero”, 43, 57, “5zero”, 1];
 
     * для рандомного числа используйте Math.floor(Math.random() * 100)
-<<<<<<< HEAD
 */ 
 let arr = [];
 for (i=0; i<10;i++){
     let a = Math.floor(Math.random() * 100);
     arr.push(a);
 }
-=======
-*/
->>>>>>> origin
+const replacer = function (arr) {
+for (let g=0; g<arr.length; g++){
+// if (arr[g].split('').indexOf(0)!==-1) {
+
+// }
+// }
+// return arr
+// }
+
+let j = arr[g].split('').ingexOf(0)
+console.log()
+// if (j==-1){
+//     continue
+// }else {
+
+// }
+}}
